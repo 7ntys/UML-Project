af01a3948c11b1a2edc2d42ad24351c442711576
Datharr <61124632+Datharr@users.noreply.github.com>
2023-11-16 16:55:37 +0100

    Create Rental Shop class

---
+++ src/RentalShop.java
@@ -0,0 +1,37 @@
+public class RentalShop {
+    private double rentalShopCA;
+    private String rentalShop;
+    private String rentalShopAdress;
+
+    RentalShop(String rentalShop, String rentalShopAdress, double rentalShopCA) {
+        this.rentalShop = rentalShop;
+        this.rentalShopAdress = rentalShopAdress;
+        this.rentalShopCA = rentalShopCA;
+    }
+
+    public double getRentalShopCA() {
+        return rentalShopCA;
+    }
+
+    public void setRentalShopCA(double rentalShopCA) {
+        this.rentalShopCA = rentalShopCA;
+    }
+
+    public String getRentalShop() {
+        return rentalShop;
+    }
+
+    public void setRentalShop(String rentalShop) {
+        this.rentalShop = rentalShop;
+    }
+
+    public String getRentalShopAdress() {
+        return rentalShopAdress;
+    }
+
+    public void setRentalShopAdress(String rentalShopAdress) {
+        this.rentalShopAdress = rentalShopAdress;
+    }
+
+    // GetListofVehicleAvailable()
+}
