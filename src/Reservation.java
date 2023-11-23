e1dec0f7a7bd3d0f73acc107c4dcf310265d5a1f
DamienSth <103647263+DamienSth@users.noreply.github.com>
2023-11-23 14:45:58 +0200

    vehicle and reservation class

---
+++ src/Reservation.java
@@ -0,0 +1,18 @@
+public class Reservation{
+    private String reservationId;
+    private String reservationDebutDate;
+    private String reservationEndDate;
+
+    public string getReservationDebutDate() {
+        return ReservationDebutDate;
+    }
+    public void setReservationDebutDate(String ReservationDebutDate){
+        this.reservationDebutDate = ReservationDebutDate;
+    }
+    public string getReservationEndDate() {
+        return ReservationEndDate;
+    }
+    public void setReservationEndDate(String ReservationEndDate){
+        this.reservationEndDate = ReservationEndDate;
+    }
+}
