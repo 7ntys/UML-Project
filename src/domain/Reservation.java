8301de23f91bc73c53477496ca71f7662225590d
DamienSth <103647263+DamienSth@users.noreply.github.com>
2023-11-30 09:51:21 +0000

    Ceci est un commit

--- src/domain/Reservation.java
+++ src/domain/Reservation.java
@@ -5,14 +5,54 @@ public class Reservation{
     private String reservationDebutDate;
     private String reservationEndDate;
 
-    public string getReservationDebutDate() {
-        return ReservationDebutDate;
+    private Reservation(Builder builder) {
+        this.reservationId = builder.reservationId;
+        this.reservationDebutDate = builder.reservationDebutDate;
+        this.reservationEndDate = builder.reservationEndDate;
+    }
+
+    @Override
+    public String toString() {
+        return "domain.Reservation{" +
+                "reservationId='" + reservationId + '\'' +
+                ", reservationDebutDate='" + reservationDebutDate + '\'' +
+                ", reservationEndDate=" + reservationEndDate +
+                '}';
+    }
+
+    public static class Builder{
+        private String reservationId;
+        private String reservationDebutDate;
+        private String reservationEndDate;
+
+        public Builder setReservationId(String reservationId) {
+            this.reservationId = reservationId;
+            return this;
+        }
+
+        public Builder setReservationDebutDate(String reservationDebutDate) {
+            this.reservationDebutDate = reservationDebutDate;
+            return this;
+        }
+
+        public Builder setReservationEndDate(String reservationEndDate) {
+            this.reservationEndDate = reservationEndDate;
+            return this;
+        }
+
+        public Reservation build(){
+            return new Reservation(this);
+        }
+    }
+
+    public String getReservationDebutDate() {
+        return reservationDebutDate;
     }
     public void setReservationDebutDate(String ReservationDebutDate){
         this.reservationDebutDate = ReservationDebutDate;
     }
-    public string getReservationEndDate() {
-        return ReservationEndDate;
+    public String getReservationEndDate() {
+        return reservationEndDate;
     }
     public void setReservationEndDate(String ReservationEndDate){
         this.reservationEndDate = ReservationEndDate;
